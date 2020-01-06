db24720075c576846ec6b338cbfe06da50fc3d6b
Chinedu Eze <chinedu.eze@magicad.com>
2020-01-06 17:08:44 +0200

    setup the main ui

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 200,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
