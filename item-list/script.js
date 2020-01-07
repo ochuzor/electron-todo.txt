efc6f41897a52d01821a06fc37f1822385ff9901
Chinedu Eze <chinedu.eze@magicad.com>
2020-01-07 11:02:02 +0200

    added a search function

--- item-list/script.js
+++ item-list/script.js
@@ -2,21 +2,39 @@
 
 const $ = require('jquery')
 const Mousetrap = require('mousetrap')
+// const _ = require('lodash')
 
-const { getAll } = require('../store')
+const {
+    getAll,
+    search
+} = require('../store')
 
-const searchForm = $(".search-bar")
 const list = $('#item-list')
+const searchInput = $('#search-input')
 
+// const onInputHandler = _.debounce(inputChangeHandler, 250, { 'maxWait': 1000 })
 const allItems = getAll()
+
+function inputChangeHandler(e) {
+    const term = searchInput.val()
+    if (term && term.trim() !== '') {
+        const items = search(term) || []
+        displayElemets(items)
+    } else {
+        displayElemets(allItems)
+    }
+}
+
+searchInput.on('input', inputChangeHandler)
 
 const getListItem = (itm) => {
     return $("<li></li>")
         .text(itm.text)
         .addClass('lst-itm')
-        .click(function(){
-            // $(this).hide();
-            console.log($(this).text())
+        .attr('data-id', itm.id)
+        .click(function() {
+            const itm = $(this)
+            console.log(itm.text(), itm.attr('data-id'))
           })
 }
 
