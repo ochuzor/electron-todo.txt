97ef730aa15c416eded290c6169c247bb7f731e5
chinedu eze <ochuzor@gmail.com>
2020-01-22 08:23:26 +0200

    implemented file store and ipc store

--- store-lib/src/index.ts
+++ store-lib/src/index.ts
@@ -1,54 +1,5 @@
-import _  from 'lodash';
-import shortid from 'shortid';
-
-import { TextIndexer } from '../../store/text-indexer';
-import FileHandler from './file-handler';
-import {
-    IDataStore, 
-    TodoIndexDoc
-} from './data-store';
-
-function textToData(text: string): TodoIndexDoc {
-    return {
-      id: shortid.generate(),
-      text
-    };
-}
-
-function isString(value: string | TodoIndexDoc): value is string {
-    return _.isString(value);
-}
-
-export class FileDataStore implements IDataStore {
-    constructor(private indexer: TextIndexer, private fileHandler: FileHandler) {}
-
-    saveText(data: string | TodoIndexDoc): Promise<TodoIndexDoc> {
-        return new Promise((resolve) => {
-            const _data = isString(data) ? textToData(data) : _.cloneDeep(data);
-            
-            this.indexer.addToIndex(_data);
-            this.fileHandler.writeData(this.indexer.toString());
-            resolve(_data);
-        });
-    }
-
-    deleteDoc(id: string): Promise<string> {
-        return new Promise(resolve => {
-            this.indexer.removeFromIndex(id);
-            this.fileHandler.writeData(this.indexer.toString());
-            resolve(id);
-        })
-    }
-
-    getAll(): TodoIndexDoc[] {
-      return this.indexer.getAll();
-    }
-
-    getItem(id: string): TodoIndexDoc {
-        return this.indexer.getDocument(id);
-    }
-
-    search(term: string): TodoIndexDoc[] {
-        return this.indexer.search(term);
-    }
-}
+export {FileDataStore} from './file-data-store';
+export {default as FileHandler} from './file-handler';
+export {IDataStore, TodoIndexDoc} from './data-store';
+export {IpcRendererDataStore} from './ipc-renderer-data-store';
+export {IpcStoreEvent} from './ipc-events-constants';
---
+++ store-lib/src/ipc-events-constants.ts
@@ -0,0 +1,7 @@
+export enum IpcStoreEvent {
+    SaveText = 'STORE_SAVE_TEXT',
+    DeleteDoc = 'STORE_DELETE_DOC',
+    GetAllDocs = 'STORE_GET_ALL_DOCS',
+    SearchDocs = 'STORE_SEARCH_DOCS',
+    GetDoc = 'STORE_GET_DOC'
+};
